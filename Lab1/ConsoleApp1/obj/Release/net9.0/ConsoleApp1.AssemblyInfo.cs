5ea7ec534a3046587b8c286d47df7035c1d86e5e
FoF312 <bulonv786@gmail.com>
2025-12-04 20:44:45 +0200

    999

--- Lab1/ConsoleApp1/obj/Release/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ Lab1/ConsoleApp1/obj/Release/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c19c8de35b97524dc735d52cee20efa9b021de8f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
